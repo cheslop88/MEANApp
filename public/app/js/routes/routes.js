b18f2f1517b9e799237efc623939e23271cc1180
cheslop88 <c.heslop88@googlemail.com>
2016-03-18 18:58:22 +0000

    Update routes.js

--- public/app/js/routes/routes.js
+++ public/app/js/routes/routes.js
@@ -3,38 +3,47 @@ angular.module('myApp').config(['$routeProvider', function($routeProvider) {
         .when('/home', {
             templateUrl: 'partials/home/index.html'
         })
+        /* Individual car page */
         .when('/cars/:id', {
             templateUrl: 'partials/cars/index.html',
             controller: 'singleCarCtrl'
         })
+        /* All stock page */
         .when('/stock', {
             templateUrl: 'partials/stock/index.html',
             controller: 'carCtrl'
         })
+        /* Admin page */
         .when('/admin', {
             templateUrl: 'partials/admin/index.html',
             controller: 'carCtrl'
         })
+        /* Sold Items page */
         .when('/admin/sold', {
             templateUrl: 'partials/admin/sold/index.html',
             controller: 'carCtrl'
         })
+        /* Deleted Confirmation page */
         .when('/admin/delete/deleted', {
             templateUrl: 'partials/admin/delete/deleted/index.html',
             controller: 'carCtrl'
         })
+        /* Individual Update page */
         .when('/admin/update/:id', {
             templateUrl: 'partials/admin/update/index.html',
             controller: 'singleCarCtrl'
         })
+        /* Update confirmation page */
         .when('/admin/updated', {
             templateUrl: 'partials/admin/update/updated/index.html',
             controller: 'singleCarCtrl'
         })
+        /* Add Page */
         .when('/admin/add', {
             templateUrl: 'partials/admin/add/index.html',
             controller: 'carCtrl'
         })
+        /* Add confirmation page */
         .when('/admin/added', {
             templateUrl: 'partials/admin/add/added/index.html',
             controller: 'carCtrl'
@@ -42,5 +51,6 @@ angular.module('myApp').config(['$routeProvider', function($routeProvider) {
         .when('/about', {
             templateUrl: 'partials/about/index.html'
         })
+        /* Any other page link provided in url, link back to home */
         .otherwise({redirectTo: '/home'});
 }]);
