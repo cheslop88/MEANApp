1e962fe748e25f657db66f4f794f9d3affb842d5
cheslop88 <c.heslop88@googlemail.com>
2016-03-18 19:01:23 +0000

    Update carousel.js

--- public/app/js/directives/carousel.js
+++ public/app/js/directives/carousel.js
@@ -1,8 +1,10 @@
+/* Directive for carousel feature */
 angular.module('myApp').directive('carouselContent', function () {
     return {
         templateUrl: 'partials/carousel/index.html',
         restrict: 'E',  
         replace: true,
+        /* Provided by Owl Carousel */
         link: function (scope, element, attrs) {  
             var options = scope.$eval($(element).attr('data-options'));
             $(element).owlCarousel(options);
