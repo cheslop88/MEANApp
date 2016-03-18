7b93f8eb7256fbaea8a97db284370d9359be3827
cheslop88 <c.heslop88@googlemail.com>
2016-03-18 19:13:34 +0000

    Update gulpfile.js

--- public/app/gulpfile.js
+++ public/app/gulpfile.js
@@ -11,6 +11,7 @@ var gulp = require('gulp'),
 
 
 gulp
+/* Concat scripts into 1 file */
 .task('concatScripts', function(){
 	gulp.src([
 		'js/app.js',
@@ -19,17 +20,22 @@ gulp
 		'js/controllers/*.js', 
 		'js/directives/*.js',
 		'js/index.js'])
+	/* Add in any missing angular include variables before minification */
 	.pipe(ngAnnotate({remove: true, add: true, single_quotes: true}))
 	.pipe(maps.init())
+	/* Concat to main.js */
 	.pipe(concat("main.js"))
 	.pipe(gulp.dest('js'))
 })
+/* Minify the created concatScripts file */
 .task('minifyScripts', function(){
 	gulp.src('js/main.js')
 	.pipe(uglify())
+	/* Create a min files of main.js */
 	.pipe(rename('main.min.js'))
 	.pipe(gulp.dest('js'))
 })
+/* Sass compile */
 .task('compileSass', function(){
   return gulp.src('css/scss/*.scss')
       .pipe(maps.init())
@@ -37,14 +43,17 @@ gulp
       .pipe(maps.write('./min'))
       .pipe(gulp.dest('css/compiled'));
 })
+/* Minify CSS output */
 .task('minify-css', function() {
   return gulp.src('css/compiled/*.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('css/compiled/min/'));
 })
+/* Watch Sass */
 .task('watchSass', function(){
   gulp.watch('css/scss/*.scss', ['compileSass']);
 })
+/* Default build task */
 .task("build", ['concatScripts', 'minifyScripts', 'compileSass', 'minify-css'])
   .task("default", ['build']);
    
